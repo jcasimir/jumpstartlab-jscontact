c042af37b7635729189fa87d71d76d4b6864238a
Jeff Casimir <jeff@casimircreative.com>
2010-09-23 13:02:10 -0400

    Added before(:each) to setup a valid person before each test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe Person do
+  before(:each) do
+    @person = Person.new(:first_name => "John",
+                               :last_name => "Doe",
+                               :phone_number => "2024605555")
+  end
+  
   it "is not valid without a first name" do
-    person = Person.new(:first_name => nil)
-    person.should_not be_valid
+    @person.first_name = nil    
+    @person.should_not be_valid
   end
   
   it "is not valid without a last name" do
-    person = Person.new(:last_name => nil)
-    person.should_not be_valid
+    @person.last_name = nil
+    @person.should_not be_valid
   end
 end
