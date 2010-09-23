40f6acee31d2e233d840ca1379d3091adfa50a3e
Jeff Casimir <jeff@casimircreative.com>
2010-09-23 14:31:25 -0400

    added Person - PhoneNumber relationships

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,8 @@ class Person < ActiveRecord::Base
   
   validates_presence_of :first_name, :last_name
   
+  has_many :phone_numbers
+  
   def to_s
     "#{last_name}, #{first_name}"
   end
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
   attr_accessible :number, :person_id
+  
+  belongs_to :person
 end
