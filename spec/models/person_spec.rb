a4008cb3d4c8766288456ebb483cc723f2c23e0d
Jeff Casimir <jeff@casimircreative.com>
2010-09-23 12:57:51 -0400

    Simple tests for first_name and last_name existance

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Person do
-  it "is not valid without a first_name"
+  it "is not valid without a first name" do
+    person = Person.new(:first_name => nil)
+    person.should_not be_valid
+  end
+  
+  it "is not valid without a last name" do
+    person = Person.new(:last_name => nil)
+    person.should_not be_valid
+  end
 end
