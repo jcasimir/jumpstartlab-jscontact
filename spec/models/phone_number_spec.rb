94936c8cc02393de1427fa75d2e5082db8160168
Jeff Casimir <jeff@casimircreative.com>
2010-09-24 09:23:23 -0400

    Added first validation test for phone_number checking that number is present

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,7 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PhoneNumber do
+  before(:each) do
+    @phone_number = PhoneNumber.new()
+  end
+  
   it "should be valid" do
-    PhoneNumber.new.should be_valid
+    @phone_number.should be_valid
+  end
+  
+  it "should not be valid without a number" do
+    @phone_number.number = nil
+    @phone_number.should_not be_valid
   end
 end
