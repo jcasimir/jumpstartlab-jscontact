5e0e38f058a231d5b6bffcd7c5773690bcac4fbc
Jeff Casimir <jeff@casimircreative.com>
2010-09-25 18:32:01 -0400

    Display files from contact show

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,6 @@
-module AttachmentsHelper
+module AttachmentsHelper  
+  def print_attachments(attachments)
+    items = attachments.collect{|attachment| "<li>#{link_to attachment, attachment}</li>"}
+    "<ul>" + items.join("\n") + "</ul>"
+  end
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -3,8 +3,8 @@ class Tagging < ActiveRecord::Base
   
   belongs_to :contact
   belongs_to :asset, :polymorphic => true
-  belongs_to :note, :foreign_key => :asset_id, :conditions => "asset_type = 'Note'"
-  belongs_to :attachment, :foreign_key => :asset_id, :conditions => "asset_type = 'Attachment'"
+  belongs_to :note, :foreign_key => :asset_id, :class_name => "Note"
+  belongs_to :attachment, :foreign_key => :asset_id, :class_name => "::Attachment"
   
   validates_presence_of :contact
   validates_presence_of :asset
