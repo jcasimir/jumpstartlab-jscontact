be56a5ecc863e25cac42b0f4d2ee0574509ec512
Jeff Casimir <jeff@casimircreative.com>
2010-09-24 22:06:20 -0400

    Generated Contact scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100924215528) do
+ActiveRecord::Schema.define(:version => 20100925020400) do
+
+  create_table "contacts", :force => true do |t|
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "email_addresses", :force => true do |t|
     t.string   "address"
